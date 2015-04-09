2c51a089692c28f01a419259452ac9a18e9f1d9d
Gregory Gold <greg.gold1@gmail.com>
2015-04-09 18:03:53 -0400

    Remove passwords
    Will encrypt them next

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
     :port                 => 587,
     :domain               => 'gmail.com',
     :user_name            => 'greg.gold1@gmail.com',
-    :password             => 'hamtaro1',
+    :password             => 'XXXXXX',
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
 end
